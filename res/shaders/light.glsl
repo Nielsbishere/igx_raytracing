d2b225aa7c674da466fee23da562c39f46037cb7
Nielsbishere <n@osomi.net>
2020-02-03 18:42:27 +0100

    Added 'reflections' (skybox color for now). Corrected spelling mistake.

--- res/shaders/light.glsl
+++ res/shaders/light.glsl
@@ -42,51 +42,54 @@ float ndfGGX(const vec3 n, const vec3 h, const float roughness) {
 	return a2 / denom;
 }
 
-//Geometry function (shlick GGX)
-float geomShlickGGX(const float NdotV, const float k) {
+//Geometry function (schlick GGX)
+float geomSchlickGGX(const float NdotV, const float k) {
 	return NdotV / (NdotV * (1 - k) + k);
 }
 
-//Geometry function (shlick GGX smith)
+//Geometry function (schlick GGX smith)
 float geomSmith(const float NdotV, const float NdotL, const float k) {
-	return geomShlickGGX(NdotV, k) * geomShlickGGX(NdotL, k);
+	return geomSchlickGGX(NdotV, k) * geomSchlickGGX(NdotL, k);
 }
 
-//Fresnel approximation (Shlick)
-vec3 fresnelShlick(const vec3 F0, const vec3 h, const vec3 v) {
+//Fresnel approximation (Schlick)
+vec3 fresnelSchlick(const vec3 F0, const vec3 h, const vec3 v) {
 	return F0 + (1 - F0) * pow(1 - dot(h, v), 5);
+}
+
+//Fresnel approximation (Schlick roughness)
+vec3 fresnelSchlickRoughness(const vec3 F0, const float NdotV, const float metallic, const vec3 albedo, const float roughness) {
+	return F0 + (max(F0, vec3(1 - roughness)) - F0) * pow(1 - NdotV, 5);
 }
 
 //Cook-torrance brdf
 
 vec3 cookTorrance(
+	const vec3 F0,
 	const vec3 albedo,
 	const Light light, 
 	const vec3 n,
 	const vec3 l,
-	const vec3 v, 
+	const vec3 v,
+	const float NdotV, 
 	const float dist,
 	const float roughness,
 	const float metallic,
-	const float k,
-	const vec3 specular
+	const float k
 ) {
 
 	const float NdotL = max(dot(n, l), 0);
-	const float NdotV = max(dot(n, v), 0);
 
 	const vec3 h = normalize(l + v);
 
-	const vec3 F0 = mix(vec3(0.04), albedo, metallic);
-
 	const float D = ndfGGX(n, h, max(roughness, minRoughness));
 	const float G = geomSmith(NdotV, NdotL, k);
-	const vec3 F = fresnelShlick(F0, h, v);
+	const vec3 F = fresnelSchlick(F0, h, v);
 
 	const vec3 kS = D * F * G / (4 * NdotL * NdotV + specularEpsilon);
 	const vec3 kD = (1 - F) * (1 - metallic);
 
-	const vec3 color = kD * albedo + kS * specular;
+	const vec3 color = kD * albedo + kS;
 
 	return color * light.col * dist * NdotL;
 }
